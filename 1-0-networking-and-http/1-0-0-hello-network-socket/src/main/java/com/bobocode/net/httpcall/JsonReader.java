28314e87b303eb2a0eef797341c03b5428eb370f
Yurii Zaluskyi <yurazaluskyi@gmail.com>
2021-10-03 19:20:46 +0300

    commit

---
+++ 1-0-networking-and-http/1-0-0-hello-network-socket/src/main/java/com/bobocode/net/httpcall/JsonReader.java
@@ -0,0 +1,33 @@
+package com.bobocode.net.httpcall;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+import lombok.SneakyThrows;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JsonReader {
+  @SneakyThrows
+  public static void main(String[] args) {
+    URL url = new URL("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY");
+    URLConnection urlConnection = url.openConnection();
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+    String str;
+    StringBuilder stringBuilder = new StringBuilder();
+    while ((str = bufferedReader.readLine()) != null)
+    {stringBuilder.append(str);}
+    bufferedReader.close();
+    Object object = new JSONParser().parse(stringBuilder.toString());
+    JSONObject jsonObject = (JSONObject) object;
+    JSONArray arrayPhotos = (JSONArray) jsonObject.get("photos");
+    Iterator photoIterator = arrayPhotos.iterator();
+    while (photoIterator.hasNext()) {
+      JSONObject photo = (JSONObject) photoIterator.next();
+      System.out.println(photo.get("img_src"));
+    }
+  }
+}
---
+++ 1-0-networking-and-http/1-0-0-hello-network-socket/src/main/java/com/bobocode/net/httpcall/JsonReaderOneMore.java
@@ -0,0 +1,36 @@
+package com.bobocode.net.httpcall;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import lombok.SneakyThrows;
+
+public class JsonReaderOneMore {
+
+  @SneakyThrows
+  public static void main(String[] args) {
+    URL url = new URL(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY");
+    URLConnection urlConnection = url.openConnection();
+    BufferedReader bufferedReader = new BufferedReader(
+        new InputStreamReader(urlConnection.getInputStream()));
+    String str;
+    StringBuilder stringBuilder = new StringBuilder();
+    while ((str = bufferedReader.readLine()) != null) {
+      stringBuilder.append(str);
+    }
+    bufferedReader.close();
+    JsonObject jsonObject = new JsonParser().parse(stringBuilder.toString()).getAsJsonObject();
+    JsonArray jsonArray = jsonObject.getAsJsonArray("photos");
+    for (int i = 0; i < jsonArray.size(); i++) {
+      String image = jsonArray.get(i).getAsJsonObject().get("img_src").getAsString();
+      System.out.println(image);
+    }
+  }
+}
