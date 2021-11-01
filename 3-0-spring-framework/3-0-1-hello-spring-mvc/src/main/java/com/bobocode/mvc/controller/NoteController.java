b3b21c6870024fa54f014a0c6879c08c8f709e41
Yurii Zaluskyi <yurazaluskyi@gmail.com>
2021-11-01 19:28:12 +0200

    commit

--- 3-0-spring-framework/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/controller/NoteController.java
+++ 3-0-spring-framework/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/controller/NoteController.java
@@ -7,7 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * {@link NoteController} is a typical controller that powers Spring MVC Notes application. This
@@ -48,5 +51,11 @@ public class NoteController {
   public String getNotes(Model model) {
     model.addAttribute("noteList", notes.getAll());
     return "notes";
+  }
+
+  @PostMapping
+  public String addNote(@RequestBody Note note) {
+    notes.add(note);
+    return "redirect:/notes";
   }
 }
